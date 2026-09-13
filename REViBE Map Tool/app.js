77c5a426def6d9bb9c854d22429369f5d0a8826b
Gerard Rick Jardin <gerardrickjardin@gmail.com>
2026-09-13 12:29:48 -0600

    fix(webhook): update LeadConnector webhook URL to a60ed162-efe3-4b22-9d69-85c523dc8df3

--- REViBE Map Tool/app.js
+++ REViBE Map Tool/app.js
@@ -528,7 +528,7 @@ function initApplicationForm() {
                 submitBtn.innerHTML = 'Submitting Application... <i class="fa-solid fa-spinner fa-spin"></i>';
             }
 
-            const webhookUrl = 'https://services.leadconnectorhq.com/hooks/yl7m9259ARgabLb2neQw/webhook-trigger/38f6dfbc-6b9f-4ced-8983-bd80dca3a5ca';
+            const webhookUrl = 'https://services.leadconnectorhq.com/hooks/yl7m9259ARgabLb2neQw/webhook-trigger/a60ed162-efe3-4b22-9d69-85c523dc8df3';
 
             const response = await fetch(webhookUrl, {
                 method: 'POST',
